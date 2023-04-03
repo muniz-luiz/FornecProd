f115691cbb0f6c3e4e0a3acf6ce6c797bde3fe61
Luiz Muniz <eusouluizeduardomuniz@gmail.com>
2023-04-03 12:24:54 -0300

    Confuração do dbconext para cada classe do Mappings e o próximo passo é inserir o dbconext na aplicação

--- FornecProd.Data/Context/FornecProdContext.cs
+++ FornecProd.Data/Context/FornecProdContext.cs
@@ -19,8 +19,18 @@ namespace FornecProd.Data.Context
         public DbSet<Endereco> Enderecos { get; set; }
         public DbSet<Fornecedor> Fornecedores { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
 
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
 
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(FornecProdContext).Assembly);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
 
     }
 }
